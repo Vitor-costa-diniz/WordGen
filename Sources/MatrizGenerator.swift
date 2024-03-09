8026071df0706d2c5c4867c8cffa94c6dbd26858
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-03-09 12:12:02 -0300

    Create funcs to setWords and sort initial positions

--- Sources/MatrizGenerator.swift
+++ Sources/MatrizGenerator.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class MatrizGenerator {
     private let matriz = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    private let wordsSorted: [Word] = []
     
     func generateMatriz(size: Int) {
         for _ in 1...size {
@@ -19,4 +20,38 @@ class MatrizGenerator {
             print("")
         }
     }
+}
+
+extension MatrizGenerator {
+    private func setWords(size: Int) {
+        let words = ["Futebol", "Nordeste",
+            "Ceara", "Ferroviario","Gols",
+            "Castelao", "Bahia", "Recife"]
+        
+        for element in words {
+            var helper = Word()
+            helper.word = element
+            helper.initPosition = sortPosition(size: size, wordSize: element.count, last: false)
+        }
+    }
+    
+    private func sortPosition(size: Int, wordSize: Int, last: Bool) -> [Int] {
+        let range: ClosedRange = (0...size)
+        let l = range.randomElement()!
+        var c = range.randomElement()!
+        
+        while c > wordSize {
+            c = range.randomElement()!
+        }
+
+        return [l,c]
+    }
+}
+
+struct Word {
+    var word: String = ""
+    // Linha x Coluna
+    var initPosition: [Int] = []
+    // Linha x Coluna
+    var lastPosition: [Int] = []
 }
