d642994abf754fef33e2f6bb8ffdc1fcf1e3a8d6
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-03-21 17:54:44 -0300

    Add Rainbow lib to project

--- Package.swift
+++ Package.swift
@@ -7,12 +7,14 @@ let package = Package(
     name: "WordGen",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/onevcat/Rainbow", .upToNextMajor(from: "4.0.0")),
     ],
     targets: [
         .executableTarget(
             name: "WordGen",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Rainbow", package: "Rainbow"),
             ]
         ),
     ]
