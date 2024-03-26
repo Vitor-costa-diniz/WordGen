dd6df79f2e0914d3e558723b5d9d92231b25f23b
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-03-26 17:05:14 -0300

    Continue documentation

--- Sources/BoardCreator/PutWords.swift
+++ Sources/BoardCreator/PutWords.swift
@@ -97,11 +97,31 @@ extension MatrizGenerator {
         }
     }
     
+    /// Responsible for verifying if the word can be placed in the randomly selected position before placing the word.
+    /// - Parameter word: The word to be verified.
+    /// - Returns: `true` if the word can be placed in that matrix space; otherwise, `false`.
+    ///
+    /// It first creates variables to store the initial row and column values of that word.
+    /// Then variables are created to control the execution of the for loop.
+    /// A switch-case is used on the word's orientation to determine if the count will increase in the column or row.
+    /// For both cases, two checks are performed:
+    /// 1. If the value at that position is empty.
+    /// 2. If the letter at that position in the matrix is equal to the same letter at the current index of the word chosen to be placed.
+    ///
+    /// If both checks are true, it increments the value of `putLetter` by 1.
+    /// At the end of the loop, the comparison between `putLetter` and the size of the word is returned. If they are equal, it means the word can be placed in that space.
+    
     private func verifyPosition(_ word: Word) -> Bool {
         let line = word.initPosition[0]
         let column = word.initPosition[1]
         let wordSize = word.name.count
+        /// The variable `putLetter` is the "theoretical" counter indicating whether that letter of the word can be placed in that space.
         var putLetter = 0
+        /// The variable `count` is the counter of the number of times the loop has iterated.
+        ///
+        /// We cannot use the loop variable directly because it ranges from the initial row/column value to the final position value.
+        ///
+        /// Therefore, it may start from 2, 3, and so on. If we were to use this value as an index in the word's array, it would result in an "index out of range" error, as the array index ranges from 0 to (N-1) elements in the array.
         var count = 0
 
         switch word.orientation {
